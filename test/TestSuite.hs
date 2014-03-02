0b25dd08386620c44a207d7f0672015d4348a9a5
Matthew Wampler-Doty <matt@w-d.org>
2014-03-02 08:50:50 -0600

    Introducing failing test

--- test/TestSuite.hs
+++ test/TestSuite.hs
@@ -3,9 +3,14 @@ module Main where
 import Test.CodeWars
 import Hamming (hammings)
 
+failingTest :: Spec
+failingTest = 
+  describe "Failing test" $
+    it "declares something baldly False" $
+      1 `shouldBe` (2 :: Int)
 
 main :: IO ()
-main = hspec $ do
+main = spec $ do
   describe "Prelude.head" $ do
     it "returns the first element of a list" $
       head [23 ..] `shouldBe` (23 :: Int)
@@ -20,3 +25,6 @@ main = hspec $ do
     it "starts with [1,2,3,4,5]" $
       take 5 hammings `shouldBe` [1,2,3,4,5]
 
+  failingTest
+  
+  -- TODO: test that stuff is correctly formatted JSON, since it's a bit hacked right now
