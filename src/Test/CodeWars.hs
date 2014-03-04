f13cc374f76ed85f828f2b4abd9c6b6b3efa833b
Matthew Wampler-Doty <matt@w-d.org>
2014-03-04 04:20:23 -0600

    Correctly formatting JSON now

--- src/Test/CodeWars.hs
+++ src/Test/CodeWars.hs
@@ -32,73 +32,27 @@ module Test.CodeWars (
 
 import Test.Hspec
 import Test.Hspec.Runner (hspecWith, defaultConfig, Config (configFormatter,configHandle))
-import Test.Hspec.Formatters (silent,
-                              Formatter (..),
-                              writeLine,
-                              getRealTime,
-                              getFailCount,
-                              formatException)
 import Test.QuickCheck hiding (reason)
 import Control.Exception (evaluate)
 import Test.HUnit (assertFailure)
 import Control.Monad (unless)
-import Text.Printf (printf)
-import Data.Aeson.Encode (encode)
-import qualified Data.ByteString.Lazy.Char8 as ByteStringLazy (unpack)
-import qualified Data.ByteString.Char8 as ByteString (unpack)
+import Data.ByteString.Char8 (pack, unpack)
 import System.Environment (withArgs)
-import Data.ByteString (pack)
 import qualified Data.Knob 
 import System.IO (hClose, IOMode( WriteMode ))
-
-codewarsFormatter :: Formatter
-codewarsFormatter =
-  silent {
-    headerFormatter = do
-       writeLine "{"
-       writeLine $ quote "output" ++ " : { "
-       
- , exampleGroupStarted = \_ nesting name ->
-     writeLine $
-     indentationFor nesting ++ quote name ++ " : { "
-
- , exampleSucceeded = \(nesting, requirement) ->
-     writeLine $
-     indentationFor nesting
-     ++ quote requirement ++ " : { \"success\": true },"
-
- , exampleFailed = \(nesting, requirement) reason ->
-     writeLine $
-     indentationFor nesting
-     ++ quote requirement ++ " : { \"success\": false,"
-     ++ " \"reason\": " ++ err reason ++ " },"
-     
- , exampleGroupDone = writeLine "   },"
- 
- , footerFormatter = do
-       writeLine "},"
-       n <- getFailCount
-       writeLine $ quote "success"
-         ++ ": " ++ if n == 0 then "true" else "false" ++ ","
-       time <- getRealTime
-       writeLine (printf "\"time\": %f" time)
-       writeLine "}"
-    }
-  where
-    quote = ByteStringLazy.unpack . encode
-    err reason = quote $ either (("uncaught exception: " ++) . formatException) id reason
-    indentationFor nesting = replicate ((1 + length nesting) * 2) ' '
-
+import Test.CodeWars.Formatter (json)
+import Data.String.Utils (replace)
 
 spec :: Spec -> IO String
 spec s = withArgs [] $ do
   knob <- Data.Knob.newKnob (pack [])
   h <- Data.Knob.newFileHandle knob "Test.CodeWars" WriteMode
-  _ <- hspecWith defaultConfig {configFormatter = codewarsFormatter, configHandle = Left h} s
+  _ <- hspecWith defaultConfig {configFormatter = json
+                               , configHandle = Left h} s
   hClose h
   bytes <- Data.Knob.getContents knob
-  return $ ByteString.unpack bytes
-  
+  let out = replace ",}" "}" $ unpack bytes
+  return out
 
 infix 1 `shouldNotBe`
 shouldNotBe :: (Eq a, Show a) => a -> a -> Expectation
--- test/TestSuite.hs
+++ test/TestSuite.hs
@@ -3,6 +3,9 @@ module Main where
 import Test.CodeWars
 import Hamming (hammings)
 import System.IO.Unsafe (unsafePerformIO)
+import Data.ByteString.Lazy.Char8 (pack)
+import Data.JSON2.Parser (parseJson)
+--import Data.JSON2 (pprint)
 
 fooPrinter :: String
 fooPrinter = unsafePerformIO $ do
@@ -15,36 +18,45 @@ failingTest =
     it "declares something baldly False" $
       1 `shouldBe` (2 :: Int)
 
-{- do
-        
+bigTest :: Spec
+bigTest =  do
+  describe "Prelude.head" $ do 
+    it "returns the first element of a list" $
+      head [23 ..] `shouldBe` (23 :: Int)
+      
+    it "returns the first element of an *arbitrary* list" $
+      property $ \x xs -> head (x:xs) == (x :: Int)
+  
+    it "throws an exception if used with an empty list" $
+      evaluate (head []) `shouldThrow` anyException
 
-    
--}
+  describe "hammings" $ do
+    it "starts with [1,2,3,4,5]" $
+      take 5 hammings `shouldBe` [1,2,3,4,5]
+
+  describe "fooPrinter" $ do
+    it "is pretending to be the pure value \"Bar\"" $
+      fooPrinter `shouldBe` "Bar"
+
+isCorrectlyFormattedJSON :: String -> Bool
+isCorrectlyFormattedJSON s =
+  either (const False) (const True) (parseJson s)
+
+jsonTest :: IO Spec
+jsonTest = do
+  out <- spec $ bigTest
+  return $ do
+    describe "JSON Controls" $ do
+      describe "1" $
+        it "is a valid JSON" $
+          "1" `shouldSatisfy` isCorrectlyFormattedJSON
+    describe "Big Test Output" $ do
+      it "is a properly formatted JSON" $
+        out `shouldSatisfy` isCorrectlyFormattedJSON
+        where
 
 main :: IO ()
 main = do
-  out <- spec $ do
-
-    describe "Prelude.head" $ do 
-      it "returns the first element of a list" $
-        head [23 ..] `shouldBe` (23 :: Int)
-      
-      it "returns the first element of an *arbitrary* list" $
-        property $ \x xs -> head (x:xs) == (x :: Int)
-  
-      it "throws an exception if used with an empty list" $
-        evaluate (head []) `shouldThrow` anyException
-
-    describe "hammings" $ do
-      it "starts with [1,2,3,4,5]" $
-        take 5 hammings `shouldBe` [1,2,3,4,5]
-
-    describe "fooPrinter" $ do
-      it "is pretending to be the pure value \"Bar\"" $
-        fooPrinter `shouldBe` "Bar"
-
-    failingTest
-                               
+  jt <- jsonTest
+  out <- spec $ do { bigTest ; jt }
   putStrLn out
-  
-  -- TODO: test that stuff is correctly formatted JSON, since it's a bit hacked right now
